ebc39158d3bbbe9bfd2e81a8c6022c09b162f9e1
maheesha517 <127947717+maheesha517@users.noreply.github.com>
2024-08-08 22:29:36 +0530

    chore: update details

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function downloadResume() {
-    window.location.href = 'resume.pdf'; 
-}
-console.log("Portfolio");
 document.addEventListener("DOMContentLoaded", () => {
     const progressBars = document.querySelectorAll(".progress-bar");
 
-progressBars.forEach(bar => {
-    const percentage = bar.getAttribute("data-percentage");
-    const progress = bar.querySelector(".progress");
-    progress.style.width = percentage + "%";
+    progressBars.forEach(bar => {
+        const percentage = bar.getAttribute("data-percentage");
+        const progress = bar.querySelector(".progress");
+        progress.style.width = percentage + "%";
+    });
 });
-});
+
+function downloadResume() {
+    window.location.href = 'resume.pdf';
+}
+
+console.log("Portfolio");
