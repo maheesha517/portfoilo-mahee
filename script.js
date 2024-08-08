46e0184186cd09c503e44199e32ad9655cb2f7a7
maheesha517 <127947717+maheesha517@users.noreply.github.com>
2024-08-08 17:57:18 +0530

    chore: add intial comit

---
+++ script.js
@@ -0,0 +1,13 @@
+function downloadResume() {
+    window.location.href = 'resume.pdf'; 
+}
+console.log("Portfolio");
+document.addEventListener("DOMContentLoaded", () => {
+    const progressBars = document.querySelectorAll(".progress-bar");
+
+progressBars.forEach(bar => {
+    const percentage = bar.getAttribute("data-percentage");
+    const progress = bar.querySelector(".progress");
+    progress.style.width = percentage + "%";
+});
+});
